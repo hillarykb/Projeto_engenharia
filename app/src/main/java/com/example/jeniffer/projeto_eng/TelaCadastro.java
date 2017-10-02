4ac25fda0f062e396ec0a398f20b9082d83eb92a
jenifferfa <Mya2277!>
2017-10-02 00:22:44 -0300

    telas e conexão

--- app/src/main/java/com/example/jeniffer/projeto_eng/TelaCadastro.java
+++ app/src/main/java/com/example/jeniffer/projeto_eng/TelaCadastro.java
@@ -1,13 +1,45 @@
 package com.example.jeniffer.projeto_eng;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class TelaCadastro extends AppCompatActivity {
+
+    EditText editUsuario,edisenha,editNome, editCargo;
+    Button btnCadastrar, btnCancelar;
+    Switch swtAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_cadastro);
+
+        editNome = (EditText)findViewById(R.id.editNome);
+        editUsuario = (EditText)findViewById(R.id.editUsuario);
+        edisenha = (EditText)findViewById(R.id.editSenha);
+        editCargo = (EditText)findViewById(R.id.editCargo);
+        btnCadastrar = (Button)findViewById(R.id.btnCadastrar);
+        btnCancelar = (Button)findViewById(R.id.btnCancelar);
+        swtAdmin = (Switch)findViewById(R.id.swtAdmin);
+
+        URL url;
+        HttpURLConnection connection = null;
+
+
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/jeniffer/projeto_eng/TelaLogin.java
+++ app/src/main/java/com/example/jeniffer/projeto_eng/TelaLogin.java
@@ -1,16 +1,29 @@
 package com.example.jeniffer.projeto_eng;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.DownloadListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class TelaLogin extends AppCompatActivity {
 
     EditText editUsuario, editSenha;
     Button btnConectar;
     TextView txtCadastro;
+
+    URL url;
+    HttpURLConnection connection = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,29 @@ public class TelaLogin extends AppCompatActivity {
         txtCadastro = (TextView)findViewById(R.id.txtCadastro);
 
 
+        txtCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent abreCadastro = new Intent(TelaLogin.this, TelaCadastro.class);
+                startActivity(abreCadastro);
+            }
+        });
 
+        btnConectar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConnectivityManager connMgr = (ConnectivityManager)
+                        getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+                if(networkInfo != null && networkInfo.isConnected()){
+                    new DownloadWebpageTask().execute(stringUrl);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Nenhuma conexão foi detectada", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
+
 }
